ddc0d330e452583c001a7830be3681542be65457
Berk Civelek <berkcvlk@gmail.com>
2021-09-30 16:16:42 +0300

    refactor: paths stroke animation

--- src/components/Path/styles.js
+++ src/components/Path/styles.js
@@ -2,14 +2,12 @@ import { styled, keyframes } from "stitches";
 
 const squiggle = keyframes({
   to: {
-    stroke: "$darkPurple",
     strokeDashoffset: "0",
   },
 });
 
 export const Path = styled("path", {
-  stroke: "transparent",
   strokeDasharray: "450",
   strokeDashoffset: "450",
-  animation: `${squiggle} 1.5s ease forwards`,
+  animation: `${squiggle} 2s ease forwards`,
 });
