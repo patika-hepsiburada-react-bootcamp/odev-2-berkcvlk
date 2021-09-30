a1334fd2a1b41bcf4d82192330bf10a68ecafef3
Berk Civelek <berkcvlk@gmail.com>
2021-10-01 00:16:21 +0300

    create: loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,12 @@
+import * as S from "./styles";
+
+const Loader = () => {
+  return (
+    <S.Loader>
+      <S.FirstChild />
+      <S.SecondChild />
+    </S.Loader>
+  );
+};
+
+export default Loader;
---
+++ src/components/Loader/styles.js
@@ -0,0 +1,37 @@
+import { styled, keyframes } from "stitches";
+
+const ripple = keyframes({
+  "0%": {
+    top: "36px",
+    left: "36px",
+    width: "0",
+    height: "0",
+    opacity: "1",
+  },
+  "100%": {
+    top: "0px",
+    left: "0px",
+    width: "72px",
+    height: "72px",
+    opacity: "0",
+  },
+});
+
+export const Loader = styled("div", {
+  display: "inline-block",
+  position: "relative",
+  width: "80px",
+  height: "80px",
+});
+
+export const FirstChild = styled("div", {
+  position: "absolute",
+  border: "4px solid #fff",
+  opacity: "1",
+  borderRadius: "50%",
+  animation: `${ripple} 1s cubic-bezier(0, 0.2, 0.8, 1) infinite`,
+});
+
+export const SecondChild = styled("div", {
+  animationDelay: "-0.5s",
+});
