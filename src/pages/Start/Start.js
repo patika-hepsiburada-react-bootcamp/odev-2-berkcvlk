7d801dd1b3c03ad019b0f91a4f27ca8a396c57eb
Berk Civelek <berkcvlk@gmail.com>
2021-10-01 00:14:45 +0300

    add: reset game

--- src/pages/Start/Start.js
+++ src/pages/Start/Start.js
@@ -1,11 +1,16 @@
+import { useContext } from "react";
+
 import { Page } from "layouts";
 import { Link, Illustration } from "components";
+import { GameContext } from "store/gameContext";
 import * as S from "./styles";
 
 const Start = () => {
+  const { reset } = useContext(GameContext);
+
   return (
     <Page Illustration={Illustration.ManSide}>
-      <Link size="large" to="/game">
+      <Link onClick={reset} size="large" to="/game">
         Start
       </Link>
       <S.Text>You know, just try not to hang the man.</S.Text>
