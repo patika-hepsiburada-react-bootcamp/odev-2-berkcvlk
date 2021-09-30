6b3696ac751a149c7bab22895176e9e59103b3a1
Berk Civelek <berkcvlk@gmail.com>
2021-10-01 00:15:20 +0300

    remove: page up animation

--- src/layouts/Page/styles.js
+++ src/layouts/Page/styles.js
@@ -1,22 +1,10 @@
-import { styled, keyframes } from "stitches";
-
-const slideInUp = keyframes({
-  from: {
-    opacity: "0.4",
-    transform: "translateY(16px)",
-  },
-  to: {
-    opacity: "1",
-    transform: "translateY(0)",
-  },
-});
+import { styled } from "stitches";
 
 export const Container = styled("div", {
   width: "100%",
   height: "100%",
   dFlex: "initial space-between",
   flexDirection: "column",
-  animation: `${slideInUp} 0.3s ease`,
 });
 
 export const Header = styled("div", {
