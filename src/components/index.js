efe82c69a5cf21a10555342d122da334c042c75a
Berk Civelek <berkcvlk@gmail.com>
2021-09-30 16:16:26 +0300

    add: comp exportation

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
+export * as Illustration from "./Illustration";
 export { default as Logo } from "./Logo/Logo";
 export { default as Gallow } from "./Gallow/Gallow";
 export { default as Path } from "./Path/Path";
 export { default as Title } from "./Title/Title";
+export { default as Link } from "./Link/Link";
