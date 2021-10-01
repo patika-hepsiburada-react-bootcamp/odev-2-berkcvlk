683832418a245ab26aa086c2af5c4a4bc27ddf0e
Berk Civelek <berkcvlk@gmail.com>
2021-10-01 14:35:03 +0300

    add: page slide up animation

--- src/layouts/Page/styles.js
+++ src/layouts/Page/styles.js
@@ -1,10 +1,22 @@
-import { styled } from "stitches";
+import { styled, keyframes } from "stitches";
+
+const slideInUp = keyframes({
+  from: {
+    opacity: "0.4",
+    transform: "translateY(16px)",
+  },
+  to: {
+    opacity: "1",
+    transform: "translateY(0)",
+  },
+});
 
 export const Container = styled("div", {
   width: "100%",
   height: "100%",
   dFlex: "initial space-between",
   flexDirection: "column",
+  animation: `${slideInUp} 0.3s ease`,
 });
 
 export const Header = styled("div", {
