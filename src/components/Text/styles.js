9cfa3aecf9394ce7a766558999496e0043cce718
Berk Civelek <berkcvlk@gmail.com>
2021-10-02 21:41:35 +0300

    add: variants for text

--- src/components/Text/styles.js
+++ src/components/Text/styles.js
@@ -1,15 +1,32 @@
 import { styled } from "stitches";
 
-export const Text = styled("p", {
+export const Text = styled("span", {
+  display: "block",
   fontSize: "$3",
   fontWeight: "500",
   color: "$gray500",
+  marginY: "12px",
 
   variants: {
     size: {
       small: {
         fontSize: "$1",
         color: "$gray600",
+      },
+    },
+    color: {
+      orange: {
+        color: "$orange",
+      },
+      gray600: {
+        color: "$gray600",
+      },
+    },
+    behaviour: {
+      inline: {
+        display: "inline-block",
+        marginY: "0",
+        marginLeft: "8px",
       },
     },
   },
